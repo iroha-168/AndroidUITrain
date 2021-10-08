105a2d1d5eebf21fdf06f63be5bf3b0d640b49ed
nemo-855 <77588574+nemo-855@users.noreply.github.com>
2021-10-08 21:54:10 +0900

    BottomNavigationViewとNavigationComponentを接続

--- app/src/main/java/com/nemo/androiduitraining/view/activity/MainActivity.kt
+++ app/src/main/java/com/nemo/androiduitraining/view/activity/MainActivity.kt
@@ -2,11 +2,24 @@ package com.nemo.androiduitraining.view.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.nemo.androiduitraining.R
+import com.nemo.androiduitraining.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setUpBottomNavigationView()
+    }
+
+    private fun setUpBottomNavigationView() {
+        val navController = findNavController(R.id.nav_host_fragment)
+        binding.bottomNavigationView.setupWithNavController(navController)
     }
 }
