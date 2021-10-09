97910a4d88d9d2646708519c8f0cdb47cf1aa57c
nemo-855 <77588574+nemo-855@users.noreply.github.com>
2021-10-09 21:52:28 +0900

    FavoriteFragmentの子FragmentのViewBindingをセットアップした

--- app/src/main/java/com/nemo/androiduitraining/view/fragment/favorite/FavoriteBrandFragment.kt
+++ app/src/main/java/com/nemo/androiduitraining/view/fragment/favorite/FavoriteBrandFragment.kt
@@ -1,6 +1,28 @@
 package com.nemo.androiduitraining.view.fragment.favorite
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.nemo.androiduitraining.databinding.FragmentFavoriteBrandBinding
 
 class FavoriteBrandFragment : Fragment() {
+    private var _binding: FragmentFavoriteBrandBinding? = null
+    private val binding: FragmentFavoriteBrandBinding
+        get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentFavoriteBrandBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
--- app/src/main/java/com/nemo/androiduitraining/view/fragment/favorite/FavoriteItemFragment.kt
+++ app/src/main/java/com/nemo/androiduitraining/view/fragment/favorite/FavoriteItemFragment.kt
@@ -1,4 +1,28 @@
 package com.nemo.androiduitraining.view.fragment.favorite
 
-class FavoriteItemFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.nemo.androiduitraining.databinding.FragmentFavoriteItemBinding
+
+class FavoriteItemFragment : Fragment() {
+    private var _binding: FragmentFavoriteItemBinding? = null
+    private val binding: FragmentFavoriteItemBinding
+        get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentFavoriteItemBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
