c2fb1df8e439dd17380af058be4a33af193418af
nemo-855 <77588574+nemo-855@users.noreply.github.com>
2021-10-09 19:19:33 +0900

    FavoriteFragmentの上部のレイアウトを作成

--- app/src/main/java/com/nemo/androiduitraining/view/fragment/FavoriteFragment.kt
+++ app/src/main/java/com/nemo/androiduitraining/view/fragment/FavoriteFragment.kt
@@ -1,6 +1,28 @@
 package com.nemo.androiduitraining.view.fragment
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.nemo.androiduitraining.databinding.FragmentFavoriteBinding
 
 class FavoriteFragment : Fragment() {
+    private var _binding: FragmentFavoriteBinding? = null
+    private val binding: FragmentFavoriteBinding
+        get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentFavoriteBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
