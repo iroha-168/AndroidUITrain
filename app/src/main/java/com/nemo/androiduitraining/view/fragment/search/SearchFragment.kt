06abbc2f6c1c7bdd94e76ca93640f82423869621
hamuyatti <syosatci@gmail.com>
2021-10-10 20:25:20 +0900

    bindingの設定

--- app/src/main/java/com/nemo/androiduitraining/view/fragment/search/SearchFragment.kt
+++ app/src/main/java/com/nemo/androiduitraining/view/fragment/search/SearchFragment.kt
@@ -1,6 +1,28 @@
 package com.nemo.androiduitraining.view.fragment.search
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.nemo.androiduitraining.R
+import com.nemo.androiduitraining.databinding.FragmentSearchBinding
 
 class SearchFragment : Fragment() {
+    private var _binding:FragmentSearchBinding? = null
+    private val binding: FragmentSearchBinding
+        get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSearchBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
 }
