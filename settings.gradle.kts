ad94d6cd7baccf64ed868355c9c5e712a024df40
nemo-855 <77588574+nemo-855@users.noreply.github.com>
2021-10-10 21:24:10 +0900

    ビルドエラー解決

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,7 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven ("https://jitpack.io")
         jcenter() // Warning: this repository is going to shut down soon
     }
 }
