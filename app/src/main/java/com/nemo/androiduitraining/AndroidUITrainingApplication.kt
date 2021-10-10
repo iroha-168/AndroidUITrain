c505d56598e4ae7cb44a3c4001d50a96a9b538c5
NaokiHidaka <dosukoroid@gmail.com>
2021-10-10 15:32:46 +0900

    setup timber

---
+++ app/src/main/java/com/nemo/androiduitraining/AndroidUITrainingApplication.kt
@@ -0,0 +1,18 @@
+package com.nemo.androiduitraining
+
+import android.app.Application
+import androidx.viewbinding.BuildConfig
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
+@HiltAndroidApp
+class AndroidUITrainingApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.asTree())
+        }
+    }
+}
