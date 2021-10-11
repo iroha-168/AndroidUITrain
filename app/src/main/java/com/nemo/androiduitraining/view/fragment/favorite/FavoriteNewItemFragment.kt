7493d3d2b3916d1e2161ae4c37f8561269329057
nemo-855 <77588574+nemo-855@users.noreply.github.com>
2021-10-11 22:43:28 +0900

    FavoriteNewItemFragmentのRecyclerViewにdividerをつけた

--- app/src/main/java/com/nemo/androiduitraining/view/fragment/favorite/FavoriteNewItemFragment.kt
+++ app/src/main/java/com/nemo/androiduitraining/view/fragment/favorite/FavoriteNewItemFragment.kt
@@ -1,11 +1,12 @@
 package com.nemo.androiduitraining.view.fragment.favorite
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.nemo.androiduitraining.R
 import com.nemo.androiduitraining.databinding.FragmentFavoriteNewItemBinding
 import com.nemo.androiduitraining.viewModel.favorite.FavoriteNewItemViewModel
@@ -44,6 +45,21 @@ class FavoriteNewItemFragment : Fragment(R.layout.fragment_favorite_new_item) {
     }
 
     private fun setupRecycler() {
-        binding.favoriteNewItemRecycler.adapter = initializeGroupieAdapter()
+        val layoutManager = LinearLayoutManager(requireContext())
+
+        binding.favoriteNewItemRecycler.also {
+            it.layoutManager = layoutManager
+            it.addItemDecoration(createRecyclerDiver(layoutManager))
+            it.adapter = initializeGroupieAdapter()
+        }
+    }
+
+    private fun createRecyclerDiver(layoutManager: LinearLayoutManager): DividerItemDecoration {
+        val dividerDrawable = ContextCompat.getDrawable(requireContext(), R.drawable.fragment_favorite_new_item_divider)
+        return DividerItemDecoration(requireContext(), layoutManager.orientation).apply {
+            if (dividerDrawable != null) {
+                this.setDrawable(dividerDrawable)
+            }
+        }
     }
 }
