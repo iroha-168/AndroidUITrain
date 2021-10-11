a4db18f08b360c3b0748f7c5d7de833fc9786243
nemo-855 <77588574+nemo-855@users.noreply.github.com>
2021-10-11 20:32:30 +0900

    FavoriteNewItemViewModelを作成

--- app/src/main/java/com/nemo/androiduitraining/view/fragment/favorite/FavoriteNewItemFragment.kt
+++ app/src/main/java/com/nemo/androiduitraining/view/fragment/favorite/FavoriteNewItemFragment.kt
@@ -5,13 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.nemo.androiduitraining.R
 import com.nemo.androiduitraining.databinding.FragmentFavoriteNewItemBinding
+import com.nemo.androiduitraining.viewModel.favorite.FavoriteNewItemViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class FavoriteNewItemFragment : Fragment(R.layout.fragment_favorite_new_item) {
     companion object {
         fun newInstance() = FavoriteNewItemFragment()
     }
+
+    private val viewModel: FavoriteNewItemViewModel by viewModels()
 
     private var _binding: FragmentFavoriteNewItemBinding? = null
     private val binding: FragmentFavoriteNewItemBinding
---
+++ app/src/main/java/com/nemo/androiduitraining/viewModel/favorite/FavoriteNewItemViewModel.kt
@@ -0,0 +1,10 @@
+package com.nemo.androiduitraining.viewModel.favorite
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class FavoriteNewItemViewModel @Inject constructor() : ViewModel() {
+
+}
