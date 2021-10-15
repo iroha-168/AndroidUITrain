215fbecd40776fba9017f3f38e20b8ee3c578afc
nemo-855 <77588574+nemo-855@users.noreply.github.com>
2021-10-16 01:09:54 +0900

    FavoriteItemFragmentでリストに表示するようのモデルクラスを定義して、ランダムなリストを作る関数を作成

--- app/src/main/java/com/nemo/androiduitraining/view/fragment/favorite/entity/FavoriteItemDiscription.kt
+++ app/src/main/java/com/nemo/androiduitraining/view/fragment/favorite/entity/FavoriteItemDiscription.kt
@@ -3,11 +3,32 @@ package com.nemo.androiduitraining.view.fragment.favorite.entity
 import android.view.View
 import com.nemo.androiduitraining.R
 import com.nemo.androiduitraining.databinding.FavoriteItemDescriptionBinding
+import com.nemo.androiduitraining.viewModel.favorite.FavoriteItemViewModel
 import com.xwray.groupie.viewbinding.BindableItem
 
-class FavoriteItemDescription : BindableItem<FavoriteItemDescriptionBinding>() {
+class FavoriteItemDescription(
+    private val displayData: FavoriteItemViewModel.DisplayClothsData
+) : BindableItem<FavoriteItemDescriptionBinding>() {
     override fun bind(viewBinding: FavoriteItemDescriptionBinding, position: Int) {
+        viewBinding.clothsNameTv.text = displayData.itemName
+        viewBinding.clothsGenreTv.text = displayData.itemGenre
+        viewBinding.clothsPriceTv.text = displayData.itemPrice.toString()
 
+        when (val percent = displayData.discountPercent) {
+            null -> viewBinding.discountPercentTv.visibility = View.GONE
+            else -> {
+                viewBinding.discountPercentTv.visibility = View.VISIBLE
+                viewBinding.discountPercentTv.text = percent.toString()
+            }
+        }
+
+        when (val price = displayData.couponPrice) {
+            null -> viewBinding.couponPriceTv.visibility = View.GONE
+            else -> {
+                viewBinding.couponPriceTv.visibility = View.VISIBLE
+                viewBinding.couponPriceTv.text = price.toString()
+            }
+        }
     }
 
     override fun getLayout() = R.layout.favorite_item_description
