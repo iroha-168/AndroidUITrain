7cf2ab4494daaeb0543a7d95e63bf935903b35b1
nemo-855 <77588574+nemo-855@users.noreply.github.com>
2021-10-16 11:06:19 +0900

    FavoriteItemCellの引数名を修正した

--- app/src/main/java/com/nemo/androiduitraining/view/fragment/favorite/entity/FavoriteItemCell.kt
+++ app/src/main/java/com/nemo/androiduitraining/view/fragment/favorite/entity/FavoriteItemCell.kt
@@ -6,12 +6,12 @@ import com.nemo.androiduitraining.databinding.FavoriteNewItemItemCellBinding
 import com.xwray.groupie.viewbinding.BindableItem
 
 class FavoriteItemCell(
-    private val itemName: String,
-    private val brandName: String
+    private val brandName: String,
+    private val brandNameJapanese: String
 ) : BindableItem<FavoriteNewItemItemCellBinding>() {
     override fun bind(viewBinding: FavoriteNewItemItemCellBinding, position: Int) {
-        viewBinding.itemNameTv.text = itemName
-        viewBinding.brandTv.text = brandName
+        viewBinding.itemNameTv.text = brandName
+        viewBinding.brandTv.text = brandNameJapanese
     }
 
     override fun getLayout() = R.layout.favorite_new_item_item_cell
