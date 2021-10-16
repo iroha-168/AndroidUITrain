d71ff43ad402fd17f37d23ee7529edcea95f75ae
nemo-855 <77588574+nemo-855@users.noreply.github.com>
2021-10-16 11:04:50 +0900

    ブランドのリスト周りのロジックをAPIを叩いて持ってきた想定に書き換えた

--- app/src/main/java/com/nemo/androiduitraining/view/fragment/favorite/FavoriteNewItemFragment.kt
+++ app/src/main/java/com/nemo/androiduitraining/view/fragment/favorite/FavoriteNewItemFragment.kt
@@ -4,14 +4,18 @@ import android.os.Bundle
 import android.view.View
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.viewbinding.ViewBinding
 import com.nemo.androiduitraining.R
 import com.nemo.androiduitraining.databinding.FragmentFavoriteNewItemBinding
 import com.nemo.androiduitraining.view.fragment.favorite.entity.FavoriteItemCell
 import com.nemo.androiduitraining.view.fragment.favorite.entity.FavoriteNoBrandRegistered
 import com.nemo.androiduitraining.view.fragment.favorite.entity.FavoriteNowPopular
+import com.nemo.androiduitraining.viewModel.favorite.FavoriteNewItemViewModel
 import com.xwray.groupie.GroupieAdapter
+import com.xwray.groupie.viewbinding.BindableItem
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -20,12 +24,15 @@ class FavoriteNewItemFragment : Fragment(R.layout.fragment_favorite_new_item) {
         fun newInstance() = FavoriteNewItemFragment()
     }
 
+    private val viewModel: FavoriteNewItemViewModel by viewModels()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val binding = FragmentFavoriteNewItemBinding.bind(view)
 
         val adapter = GroupieCustomAdapter()
         setupRecycler(binding, adapter)
+        fetchBrandsItemAndUpdateList(adapter)
     }
 
     private fun setupRecycler(binding: FragmentFavoriteNewItemBinding, adapter: GroupieCustomAdapter) {
@@ -36,8 +43,6 @@ class FavoriteNewItemFragment : Fragment(R.layout.fragment_favorite_new_item) {
             it.addItemDecoration(createRecyclerDiver(layoutManager))
             it.adapter = adapter
         }
-
-        adapter.updateList()
     }
 
     private fun createRecyclerDiver(layoutManager: LinearLayoutManager): DividerItemDecoration {
@@ -49,23 +54,29 @@ class FavoriteNewItemFragment : Fragment(R.layout.fragment_favorite_new_item) {
         }
     }
 
+    private fun fetchBrandsItemAndUpdateList(adapter: GroupieCustomAdapter) {
+        viewModel.brandLD.observe(viewLifecycleOwner) { brands ->
+            adapter.updateList(brands)
+        }
+        viewModel.fetchBrandItems()
+    }
+
     private class GroupieCustomAdapter : GroupieAdapter() {
+        private var itemList: List<BindableItem<out ViewBinding>> = listOf()
+
         private val defaultViewList = mutableListOf(
             FavoriteNoBrandRegistered(),
             FavoriteNowPopular()
         )
 
-        private fun createItemCells() {
-            for (i in 0..29) {
-                defaultViewList.add(
-                    FavoriteItemCell("ほげほげ", "ふがふが")
+        fun updateList(brandsList: List<FavoriteNewItemViewModel.BrandItem>) {
+            this.itemList = defaultViewList + brandsList.map {
+                FavoriteItemCell(
+                    itemName = it.name,
+                    brandName = it.genre
                 )
             }
-        }
-
-        fun updateList() {
-            createItemCells()
-            update(defaultViewList)
+            update(this.itemList)
         }
     }
 }
--- app/src/main/java/com/nemo/androiduitraining/viewModel/favorite/FavoriteNewItemViewModel.kt
+++ app/src/main/java/com/nemo/androiduitraining/viewModel/favorite/FavoriteNewItemViewModel.kt
@@ -1,5 +1,7 @@
 package com.nemo.androiduitraining.viewModel.favorite
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.viewbinding.ViewBinding
 import com.nemo.androiduitraining.view.fragment.favorite.entity.FavoriteItemCell
@@ -11,5 +13,22 @@ import javax.inject.Inject
 
 @HiltViewModel
 class FavoriteNewItemViewModel @Inject constructor() : ViewModel() {
+    private val _brandsLD = MutableLiveData<List<BrandItem>>()
+    val brandLD: LiveData<List<BrandItem>>
+        get() = _brandsLD
 
+    fun fetchBrandItems() {
+        val brandsList = mutableListOf<BrandItem>()
+        for (i in 0..29) {
+            brandsList.add(
+                BrandItem("ほげほげ", "フガフガ")
+            )
+        }
+        _brandsLD.postValue(brandsList)
+    }
+
+    data class BrandItem(
+        val name: String,
+        val genre: String
+    )
 }
