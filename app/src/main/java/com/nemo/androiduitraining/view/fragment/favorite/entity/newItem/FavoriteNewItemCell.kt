2f90c759e7bf11a92c09dc4e3096bd0a90a88ece
nemo-855 <77588574+nemo-855@users.noreply.github.com>
2021-10-16 13:51:56 +0900

    favorite_new_item_item_cell margin修正

--- app/src/main/java/com/nemo/androiduitraining/view/fragment/favorite/entity/newItem/FavoriteNewItemCell.kt
+++ app/src/main/java/com/nemo/androiduitraining/view/fragment/favorite/entity/newItem/FavoriteNewItemCell.kt
@@ -11,18 +11,11 @@ import com.xwray.groupie.viewbinding.BindableItem
 
 class FavoriteNewItemCell(
     private val brandName: String,
-    private val brandNameJapanese: String,
-    private val res: Resources
+    private val brandNameJapanese: String
 ) : BindableItem<FavoriteNewItemItemCellBinding>() {
     override fun bind(viewBinding: FavoriteNewItemItemCellBinding, position: Int) {
         viewBinding.itemNameTv.text = brandName
         viewBinding.brandTv.text = brandNameJapanese
-
-        val layoutParams = ViewGroup.MarginLayoutParams(MATCH_PARENT, WRAP_CONTENT).apply {
-            this.marginStart = res.getDimension(R.dimen.side_margin_16).toInt()
-            this.marginEnd = res.getDimension(R.dimen.side_margin_16).toInt()
-        }
-        viewBinding.root.layoutParams = layoutParams
     }
 
     override fun getLayout() = R.layout.favorite_new_item_item_cell
