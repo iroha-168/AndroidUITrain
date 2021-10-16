4d920816a4b22c3f122ad18fc76c23d2e3fe2336
mi_iroha <chimosy21@gmail.com>
2021-10-16 21:57:52 +0900

    chore:コードレビュー指摘事項修正

--- app/src/main/java/com/nemo/androiduitraining/view/fragment/home/HomeFragment.kt
+++ app/src/main/java/com/nemo/androiduitraining/view/fragment/home/HomeFragment.kt
@@ -6,23 +6,18 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.nemo.androiduitraining.R
 import com.nemo.androiduitraining.databinding.FragmentHomeBinding
 
-class HomeFragment : Fragment() {
-    private lateinit var binding: FragmentHomeBinding
+class HomeFragment : Fragment(R.layout.fragment_home) {
+    private var _binding: FragmentHomeBinding? = null
+    private val binding: FragmentHomeBinding
+    get() = _binding!!
     var sampleTextList: ArrayList<String> = ArrayList()
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        binding = FragmentHomeBinding.inflate(inflater, container, false)
-        return binding.root
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentHomeBinding.bind(view)
 
         // RecyclerViewに表示するサンプルデータを作成
         createSampleData()
@@ -31,8 +26,13 @@ class HomeFragment : Fragment() {
         binding.containerForRecyclerView.layoutManager = LinearLayoutManager(context)
 
         binding.containerForRecyclerView.adapter = SampleAdapter(sampleTextList)
-
     }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
 
     private fun createSampleData() {
         for (i in 1..10) {
