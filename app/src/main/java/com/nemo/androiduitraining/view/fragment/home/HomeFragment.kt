c599871cb28914f51939d3d9d3bc3886840f4c30
mi_iroha <chimosy21@gmail.com>
2021-10-16 16:26:14 +0900

    feat:Toolbarの作成

--- app/src/main/java/com/nemo/androiduitraining/view/fragment/home/HomeFragment.kt
+++ app/src/main/java/com/nemo/androiduitraining/view/fragment/home/HomeFragment.kt
@@ -1,6 +1,45 @@
 package com.nemo.androiduitraining.view.fragment.home
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.nemo.androiduitraining.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+    private lateinit var binding: FragmentHomeBinding
+    var sampleTextList: ArrayList<String> = ArrayList()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // RecyclerViewに表示するサンプルデータを作成
+        createSampleData()
+
+        // Adapterの設定(仮)
+        binding.containerForRecyclerView.layoutManager = LinearLayoutManager(context)
+
+        binding.containerForRecyclerView.adapter = SampleAdapter(sampleTextList)
+
+    }
+
+    private fun createSampleData() {
+        for (i in 1..10) {
+            sampleTextList.add(i.toString())
+        }
+    }
 }
+
+
+
