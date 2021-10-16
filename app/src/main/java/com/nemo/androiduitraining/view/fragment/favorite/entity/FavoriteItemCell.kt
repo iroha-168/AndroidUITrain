1d7b4bfa2776b268b964615677e767f145e538cb
nemo-855 <77588574+nemo-855@users.noreply.github.com>
2021-10-16 11:29:51 +0900

    FavoriteItemCellで動的に左右のmarginをつけるようにした

--- app/src/main/java/com/nemo/androiduitraining/view/fragment/favorite/entity/FavoriteItemCell.kt
+++ app/src/main/java/com/nemo/androiduitraining/view/fragment/favorite/entity/FavoriteItemCell.kt
@@ -1,17 +1,28 @@
 package com.nemo.androiduitraining.view.fragment.favorite.entity
 
+import android.content.res.Resources
 import android.view.View
+import android.view.ViewGroup
+import android.view.ViewGroup.LayoutParams.MATCH_PARENT
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
 import com.nemo.androiduitraining.R
 import com.nemo.androiduitraining.databinding.FavoriteNewItemItemCellBinding
 import com.xwray.groupie.viewbinding.BindableItem
 
 class FavoriteItemCell(
     private val brandName: String,
-    private val brandNameJapanese: String
+    private val brandNameJapanese: String,
+    private val res: Resources
 ) : BindableItem<FavoriteNewItemItemCellBinding>() {
     override fun bind(viewBinding: FavoriteNewItemItemCellBinding, position: Int) {
         viewBinding.itemNameTv.text = brandName
         viewBinding.brandTv.text = brandNameJapanese
+
+        val layoutParams = ViewGroup.MarginLayoutParams(MATCH_PARENT, WRAP_CONTENT).apply {
+            this.marginStart = res.getDimension(R.dimen.side_margin_16).toInt()
+            this.marginEnd = res.getDimension(R.dimen.side_margin_16).toInt()
+        }
+        viewBinding.root.layoutParams = layoutParams
     }
 
     override fun getLayout() = R.layout.favorite_new_item_item_cell
