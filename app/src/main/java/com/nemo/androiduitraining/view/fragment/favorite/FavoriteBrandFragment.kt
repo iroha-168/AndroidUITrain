7dd6aa9f40061b75bc8206b870bce73a1e6b122b
nemo-855 <77588574+nemo-855@users.noreply.github.com>
2021-10-17 09:33:04 +0900

    FavoriteBrandFragmentにDataBindingを適用

--- app/src/main/java/com/nemo/androiduitraining/view/fragment/favorite/FavoriteBrandFragment.kt
+++ app/src/main/java/com/nemo/androiduitraining/view/fragment/favorite/FavoriteBrandFragment.kt
@@ -4,26 +4,25 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.nemo.androiduitraining.R
 import com.nemo.androiduitraining.databinding.FragmentFavoriteBrandBinding
+import com.nemo.androiduitraining.viewModel.favorite.FavoriteBrandViewModel
 
 class FavoriteBrandFragment : Fragment(R.layout.fragment_favorite_brand) {
     companion object {
         fun newInstance() = FavoriteBrandFragment()
     }
 
-    private var _binding: FragmentFavoriteBrandBinding? = null
-    private val binding: FragmentFavoriteBrandBinding
-        get() = _binding!!
+    private val viewModel: FavoriteBrandViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        _binding = FragmentFavoriteBrandBinding.bind(view)
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+        val binding = FragmentFavoriteBrandBinding.bind(view).also {
+            it.lifecycleOwner = viewLifecycleOwner
+            it.viewModel = viewModel
+        }
     }
 }
