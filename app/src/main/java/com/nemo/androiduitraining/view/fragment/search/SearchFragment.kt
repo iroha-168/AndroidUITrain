49027a294a00b5b68faf0c7e986247ae97320618
hamuyatti <syosatci@gmail.com>
2021-10-23 00:56:25 +0900

    レイアウトの枠組みを追加

--- app/src/main/java/com/nemo/androiduitraining/view/fragment/search/SearchFragment.kt
+++ app/src/main/java/com/nemo/androiduitraining/view/fragment/search/SearchFragment.kt
@@ -3,9 +3,10 @@ package com.nemo.androiduitraining.view.fragment.search
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
+import com.nemo.androiduitraining.R
 import com.nemo.androiduitraining.databinding.FragmentSearchBinding
 
-class SearchFragment : Fragment() {
+class SearchFragment : Fragment(R.layout.fragment_search) {
     private var _binding:FragmentSearchBinding? = null
     private val binding: FragmentSearchBinding
         get() = _binding!!
@@ -15,13 +16,9 @@ class SearchFragment : Fragment() {
         setHasOptionsMenu(true)
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        _binding = FragmentSearchBinding.inflate(inflater,container,false)
-        return binding.root
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentSearchBinding.bind(view)
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater){
