0b542d8ff32218abb582522f4a24329066a73059
uhooi <the_uhooi@yahoo.co.jp>
2021-10-24 21:24:36 +0900

    Refactor inflate

--- app/src/main/java/com/nemo/androiduitraining/view/fragment/other/OtherFragment.kt
+++ app/src/main/java/com/nemo/androiduitraining/view/fragment/other/OtherFragment.kt
@@ -6,24 +6,21 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.nemo.androiduitraining.R
 import com.nemo.androiduitraining.databinding.FragmentOtherBinding
 import com.nemo.androiduitraining.viewModel.other.OtherViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class OtherFragment : Fragment() {
+class OtherFragment : Fragment(R.layout.fragment_other) {
     private val viewModel: OtherViewModel by viewModels()
 
     private var _binding: FragmentOtherBinding? = null
     private val binding get() = _binding!!
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        _binding = FragmentOtherBinding.inflate(inflater, container, false)
-        return binding.root
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentOtherBinding.bind(view)
     }
 
     override fun onDestroyView() {
