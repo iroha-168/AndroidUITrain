1cabd0565df6b5d00745bcfaea8334cce628f840
uhooi <the_uhooi@yahoo.co.jp>
2021-10-24 21:16:21 +0900

    Add OtherViewModel

--- app/src/main/java/com/nemo/androiduitraining/view/fragment/other/OtherFragment.kt
+++ app/src/main/java/com/nemo/androiduitraining/view/fragment/other/OtherFragment.kt
@@ -1,5 +1,34 @@
 package com.nemo.androiduitraining.view.fragment.other
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.nemo.androiduitraining.databinding.FragmentOtherBinding
+import com.nemo.androiduitraining.viewModel.other.OtherViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
-class OtherFragment : Fragment()
+@AndroidEntryPoint
+class OtherFragment : Fragment() {
+    private val viewModel: OtherViewModel by viewModels()
+
+    private var _binding: FragmentOtherBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentOtherBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
---
+++ app/src/main/java/com/nemo/androiduitraining/viewModel/other/OtherViewModel.kt
@@ -0,0 +1,12 @@
+package com.nemo.androiduitraining.viewModel.other
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class OtherViewModel @Inject constructor(
+) : ViewModel() {
+    init {
+    }
+}
