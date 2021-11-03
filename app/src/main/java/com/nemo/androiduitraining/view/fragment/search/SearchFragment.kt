06ef37d1df2cd9d98c3e7a084d511d874528e88a
Dosukoi <62977464+Naoki-Hidaka@users.noreply.github.com>
2021-11-03 22:21:21 +0900

    Revert "Make base of search fragment"

--- app/src/main/java/com/nemo/androiduitraining/view/fragment/search/SearchFragment.kt
+++ app/src/main/java/com/nemo/androiduitraining/view/fragment/search/SearchFragment.kt
@@ -1,25 +1,5 @@
 package com.nemo.androiduitraining.view.fragment.search
 
-import android.os.Bundle
-import android.view.*
 import androidx.fragment.app.Fragment
-import com.nemo.androiduitraining.R
-import com.nemo.androiduitraining.databinding.FragmentSearchBinding
 
-class SearchFragment : Fragment(R.layout.fragment_search) {
-    private var _binding:FragmentSearchBinding? = null
-    private val binding: FragmentSearchBinding
-        get() = _binding!!
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        _binding = FragmentSearchBinding.bind(view)
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
-
-
-}
+class SearchFragment : Fragment()
